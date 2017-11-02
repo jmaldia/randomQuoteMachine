b6be1c25105918155cf7f63f10196efef069bfa8
Jon Maldia <jon@maldia.net>
2017-11-02 19:59:32 -0400

    Finalize JS script that gets random quote

--- js/main.js
+++ js/main.js
@@ -1,32 +1,25 @@
 $(document).ready(function() {
 
+    // This gets the random quote from forismatic on load 
+    $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?").done(update).fail(handleErr);
+
+    // This gets the random quote from forismatic on click (new quote button or tweet button)
     $('#button-new-quote').click(function() {
-      $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?")
-        .done(update)
-        .fail(handleErr);
+      $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?").done(update).fail(handleErr);
     });
 
+    // Function to update the quotes on the tweet and on the page
     function update(response) {
       var randomQuote = JSON.stringify(response.quoteText);
+      var randomQuoteAuthor = JSON.stringify(response.quoteAuthor);
       
-      var newTweet = `<a class="twitter-share-button tweet"
-            href="https://twitter.com/share"
-            data-size="large"
-            data-text="${randomQuote}"
-            data-url="https://jonmaldia.com/randomquotes"
-            data-hashtags="quotes,inspiration"
-            data-via="jonmaldia"
-            data-related="twitterapi,twitter">
-            Tweet
-            </a>
-            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>`;
-
-
-      
+      // Updates the page quote
       $('#quote').html('<section id="quote"><p>' + randomQuote + '</section></p>');
-      $("#tweet").html(newTweet);
+      // Updates the twitter URL with current quote
+      $('#tweet_btn').attr('href', 'https://twitter.com/intent/tweet?text=' + randomQuote + '&via=jonmaldia&url=http%3A%2F%2Fbit.ly%2Fjonquotes&hashtags=quotes%2Cinspiration' + "%0a-" + randomQuoteAuthor);
     }
 
+    // Error handler
     function handleErr(jqxhr, textStatus, err) {
       console.log("Request Failed: " + textStatus + ", " + err);
     }
